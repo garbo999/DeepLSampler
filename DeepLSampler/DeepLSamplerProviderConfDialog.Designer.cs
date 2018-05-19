04183d0f12d1f8f3b84468cea0c95c69e2e01620
garbo999 <garyhess999@hotmail.com>
2018-05-19 16:01:49 +0200

    Added form ...

---
+++ DeepLSampler/DeepLSamplerProviderConfDialog.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DeepLSampler
+{
+    partial class DeepLSamplerProviderConfDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 215);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test DeepL connection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Target text:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source text:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(427, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Translate source (DE-EN)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btn_OK.Location = new System.Drawing.Point(409, 291);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancel.Location = new System.Drawing.Point(506, 291);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 2;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // DeepLSamplerProviderConfDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Cancel;
+            this.ClientSize = new System.Drawing.Size(611, 361);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DeepLSamplerProviderConfDialog";
+            this.Text = "DeepL Sampler Provider Options";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
---
+++ DeepLSampler/DeepLSamplerTranslationProviderWinFormsUI.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Sdl.LanguagePlatform.Core;
+using Sdl.LanguagePlatform.TranslationMemory;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+using DeepLSampler;
+
+namespace DeepLSampler
+{
+    [TranslationProviderWinFormsUi(Id = "DeepLSamplerTranslationProviderWinFormsUI",
+                                   Name = "DeepLSamplerTranslationProviderWinFormsUI",
+                                   Description = "DeepLSamplerTranslationProviderWinFormsUI")]
+    class DeepLSamplerTranslationProviderWinFormsUI : ITranslationProviderWinFormsUI
+    {
+        #region ITranslationProviderWinFormsUI Members
+
+        public ITranslationProvider[] Browse(IWin32Window owner, LanguagePair[] languagePairs, ITranslationProviderCredentialStore credentialStore)
+        {
+            //throw new NotImplementedException();
+            DeepLSamplerProviderConfDialog dialog = new DeepLSamplerProviderConfDialog(new DeepLSamplerTranslationOptions());
+            if (dialog.ShowDialog(owner) == DialogResult.OK)
+            {
+                DeepLSamplerTranslationProvider testProvider = new DeepLSamplerTranslationProvider(dialog.Options);
+                return new ITranslationProvider[] { testProvider };
+            }
+            return null;
+        }
+
+        public bool Edit(IWin32Window owner, ITranslationProvider translationProvider, LanguagePair[] languagePairs, ITranslationProviderCredentialStore credentialStore)
+        {
+            //throw new NotImplementedException();
+            DeepLSamplerTranslationProvider editProvider = translationProvider as DeepLSamplerTranslationProvider;
+            if (editProvider == null)
+            {
+                return false;
+            }
+
+            DeepLSamplerProviderConfDialog dialog = new DeepLSamplerProviderConfDialog(editProvider.Options);
+            if (dialog.ShowDialog(owner) == DialogResult.OK)
+            {
+                editProvider.Options = dialog.Options;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool GetCredentialsFromUser(IWin32Window owner, Uri translationProviderUri, string translationProviderState, ITranslationProviderCredentialStore credentialStore)
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public TranslationProviderDisplayInfo GetDisplayInfo(Uri translationProviderUri, string translationProviderState)
+        {
+            //throw new NotImplementedException();
+            TranslationProviderDisplayInfo info = new TranslationProviderDisplayInfo();
+            info.Name = PluginResources.Plugin_NiceName;
+            info.TranslationProviderIcon = PluginResources.band_aid_icon;
+            info.TooltipText = PluginResources.Plugin_Tooltip;
+
+            info.SearchResultImage = PluginResources.band_aid_symbol;
+
+            return info;
+
+        }
+
+        public bool SupportsEditing
+        {
+            //get { throw new NotImplementedException(); }
+            get { return true; }
+        }
+
+        public bool SupportsTranslationProviderUri(Uri translationProviderUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TypeDescription
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string TypeName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
